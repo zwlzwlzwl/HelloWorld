4de618432f1d002343293eac339367353e8a5bf6
zwlzwlzwl <897358330@qq.com>
2016-10-09 00:13:09 +0800

    mmmmm

--- src/com/atguigu/Hello.java
+++ src/com/atguigu/Hello.java
@@ -3,7 +3,7 @@ package com.atguigu;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hi...wanting");
+		System.out.println("hi...meiyuan");
 		System.out.println("hello...");
 		System.out.println("bbbbbb......");
 		System.out.println("cccccc......");
